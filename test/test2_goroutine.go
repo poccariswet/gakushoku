b94cdc1a5579d71eb259f86170b3792d6dc6914d
soeyusuke <s1240231@gmail.com>
2017-06-04 17:25:34 +0900

    success test

--- test/test2_goroutine.go
+++ test/test2_goroutine.go
@@ -2,84 +2,87 @@ package main
 
 import (
 	"fmt"
-	"time"
 
 	"github.com/PuerkitoBio/goquery"
 )
 
 //[]string
-func GetmenuMon(url string, c chan []string) {
+func GetmenuMon(url string, c1 chan []string) {
 	doc, _ := goquery.NewDocument(url)
 	var m []string
 	doc.Find("div > div > section > table > tbody > tr").Each(func(_ int, s *goquery.Selection) {
 		t := s.Find("td").First().Text()
 		m = append(m, t)
 	})
-	c <- m
+	c1 <- m
 	// return m
 }
 
-func GetmenuTue(url string, c chan []string) {
+func GetmenuTue(url string, c2 chan []string) {
 	doc, _ := goquery.NewDocument(url)
 	var m []string
 	doc.Find("div > div > section > table > tbody > tr").Each(func(_ int, s *goquery.Selection) {
 		t := s.Find("td").First().Next().Text()
 		m = append(m, t)
 	})
-	time.Sleep(100 * time.Millisecond) //Mondayのgetを待ってる
-	c <- m
+	// time.Sleep(100 * time.Millisecond) //Mondayのgetを待ってる
+	c2 <- m
 	// return m
 }
 
-func GetmenuWen(url string, c chan []string) {
+func GetmenuWen(url string, c3 chan []string) {
 	doc, _ := goquery.NewDocument(url)
 	var m []string
 	doc.Find("div > div > section > table > tbody > tr").Each(func(_ int, s *goquery.Selection) {
 		t := s.Find("td").First().Next().Next().Text()
 		m = append(m, t)
 	})
-	time.Sleep(200 * time.Millisecond) //Mondayのgetを待ってる
-	c <- m
+	// time.Sleep(200 * time.Millisecond) //Mondayのgetを待ってる
+	c3 <- m
 	// return m
 }
 
-func GetmenuThu(url string, c chan []string) {
+func GetmenuThu(url string, c4 chan []string) {
 	doc, _ := goquery.NewDocument(url)
 	var m []string
 	doc.Find("div > div > section > table > tbody > tr").Each(func(_ int, s *goquery.Selection) {
 		t := s.Find("td").First().Next().Next().Next().Text()
 		m = append(m, t)
 	})
-	time.Sleep(300 * time.Millisecond) //Mondayのgetを待ってる
-	c <- m
+	// time.Sleep(300 * time.Millisecond) //Mondayのgetを待ってる
+	c4 <- m
 	// return m
 }
 
-func GetmenuFri(url string, c chan []string) {
+func GetmenuFri(url string, c5 chan []string) {
 	doc, _ := goquery.NewDocument(url)
 	var m []string
 	doc.Find("div > div > section > table > tbody > tr").Each(func(_ int, s *goquery.Selection) {
 		t := s.Find("td").First().Next().Next().Next().Next().Text()
 		m = append(m, t)
 	})
-	time.Sleep(400 * time.Millisecond) //Mondayのgetを待ってる
-	c <- m
+	// time.Sleep(400 * time.Millisecond) //Mondayのgetを待ってる
+	c5 <- m
 	// return m
 }
 
 //並列処理でそれぞれの曜日のmenuをとってくる
 func main() {
 	url := "http://www.gakushoku.com/univ_mn2.php"
-	c := make(chan []string)
-	defer close(c)
-	go GetmenuMon(url, c)
-	go GetmenuTue(url, c)
-	go GetmenuWen(url, c)
-	go GetmenuThu(url, c)
-	go GetmenuFri(url, c)
-	res1, res2, res3, res4, res5 := <-c, <-c, <-c, <-c, <-c
+	c1 := make(chan []string)
+	c2 := make(chan []string)
+	c3 := make(chan []string)
+	c4 := make(chan []string)
+	c5 := make(chan []string)
+	// defer close(c)
+	go GetmenuMon(url, c1)
+	go GetmenuTue(url, c2)
+	go GetmenuWen(url, c3)
+	go GetmenuThu(url, c4)
+	go GetmenuFri(url, c5)
+	res1, res2, res3, res4, res5 := <-c1, <-c2, <-c3, <-c4, <-c5
 
-	// fmt.Println(res4[4])
+	// fmt.Println(res1[0])
 	for _, m1 := range res1 {
 		fmt.Println(m1)
 		// time.Sleep(2 * time.Second)
@@ -100,4 +103,10 @@ func main() {
 	for _, m5 := range res5 {
 		fmt.Println(m5)
 	}
+
+	defer close(c1)
+	defer close(c2)
+	defer close(c3)
+	defer close(c4)
+	defer close(c5)
 }
