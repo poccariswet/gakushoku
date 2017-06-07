1e4845fdf7af11b8e698dd7463edb292f369b092
soeyusuke <s1240231@gmail.com>
2017-06-07 16:09:25 +0900

    update updateCafe method

--- cafe/updateCafe.go
+++ cafe/updateCafe.go
@@ -14,7 +14,7 @@ type menus struct {
 	Higawari   string `json:"higawari"`
 	Donmono    string `json:"donmono"`
 	Fish       string `json:"fish"`
-	Salada      string `json:"salada"`
+	Salada     string `json:"salada"`
 	Dessert    string `json:"desert"`
 	Onepin     string `json:"onepin"`
 	Pasta      string `json:"pasta"`
@@ -22,147 +22,37 @@ type menus struct {
 	EveningSet string `json:"eveningset"`
 }
 
-//[]string
-func GetmenuMon(url string, c1 chan []string) {
+const url = "http://www.gakushoku.com/univ_mn2.php"
+
+var menu []menus
+
+func getCafe() {
+	weeks := []string{"Monday\n", "Tuesday\n", "Wednesday\n", "Thursday\n", "Friday\n"}
 	doc, _ := goquery.NewDocument(url)
-	var m []string
-	doc.Find("div > div > section > table > tbody > tr").Each(func(_ int, s *goquery.Selection) {
-		t := s.Find("td").First().Text()
-		m = append(m, t)
+	var meshi []string
+	doc.Find("td").Each(func(_ int, s *goquery.Selection) {
+		data := s.First().Text()
+		meshi = append(meshi, data+"\n")
 	})
-	c1 <- m
-	// return m
+	for i := 0; i < 5; i++ {
+		menu = append(menu, menus{
+			Week:       weeks[i],
+			Higawari:   meshi[i],
+			Donmono:    meshi[i+5],
+			Fish:       meshi[i+10],
+			Salad:      meshi[i+15],
+			Dessert:    meshi[i+20],
+			Onepin:     meshi[i+25],
+			Pasta:      meshi[i+30],
+			Menrui:     meshi[i+35],
+			EveningSet: meshi[i+40],
+		})
+	}
 }
 
-func GetmenuTue(url string, c2 chan []string) {
-	doc, _ := goquery.NewDocument(url)
-	var m []string
-	doc.Find("div > div > section > table > tbody > tr").Each(func(_ int, s *goquery.Selection) {
-		t := s.Find("td").First().Next().Text()
-		m = append(m, t)
-	})
-	c2 <- m
-	// return m
-}
-
-func GetmenuWen(url string, c3 chan []string) {
-	doc, _ := goquery.NewDocument(url)
-	var m []string
-	doc.Find("div > div > section > table > tbody > tr").Each(func(_ int, s *goquery.Selection) {
-		t := s.Find("td").First().Next().Next().Text()
-		m = append(m, t)
-	})
-	c3 <- m
-	// return m
-}
-
-func GetmenuThu(url string, c4 chan []string) {
-	doc, _ := goquery.NewDocument(url)
-	var m []string
-	doc.Find("div > div > section > table > tbody > tr").Each(func(_ int, s *goquery.Selection) {
-		t := s.Find("td").First().Next().Next().Next().Text()
-		m = append(m, t)
-	})
-	c4 <- m
-	// return m
-}
-
-func GetmenuFri(url string, c5 chan []string) {
-	doc, _ := goquery.NewDocument(url)
-	var m []string
-	doc.Find("div > div > section > table > tbody > tr").Each(func(_ int, s *goquery.Selection) {
-		t := s.Find("td").First().Next().Next().Next().Next().Text()
-		m = append(m, t)
-	})
-	c5 <- m
-	// return m
-}
-
-//並列処理でそれぞれの曜日のmenuをとってくる
 func UpdateCafe() {
-	var menu []menus //menuのjson型構造体
-	url := "http://www.gakushoku.com/univ_mn2.php"
-	c1 := make(chan []string)
-	c2 := make(chan []string)
-	c3 := make(chan []string)
-	c4 := make(chan []string)
-	c5 := make(chan []string)
-
-	go GetmenuMon(url, c1)
-	go GetmenuTue(url, c2)
-	go GetmenuWen(url, c3)
-	go GetmenuThu(url, c4)
-	go GetmenuFri(url, c5)
-	mon, tue, wen, thu, fri := <-c1, <-c2, <-c3, <-c4, <-c5
-
-	menu = append(menu, menus{
-		Week:       "Monday",
-		Higawari:   mon[1],
-		Donmono:    mon[2],
-		Fish:       mon[3],
-		Salad:      mon[4],
-		Dessert:    mon[5],
-		Onepin:     mon[6],
-		Pasta:      mon[7],
-		Menrui:     mon[8],
-		EveningSet: mon[9],
-	})
-
-	menu = append(menu, menus{
-		Week:       "Tuesday",
-		Higawari:   tue[1],
-		Donmono:    tue[2],
-		Fish:       tue[3],
-		Salad:      tue[4],
-		Dessert:    tue[5],
-		Onepin:     tue[6],
-		Pasta:      tue[7],
-		Menrui:     tue[8],
-		EveningSet: tue[9],
-	})
-
-	menu = append(menu, menus{
-		Week:       "Wednesday",
-		Higawari:   wen[1],
-		Donmono:    wen[2],
-		Fish:       wen[3],
-		Salad:      wen[4],
-		Dessert:    wen[5],
-		Onepin:     wen[6],
-		Pasta:      wen[7],
-		Menrui:     wen[8],
-		EveningSet: wen[9],
-	})
-
-	menu = append(menu, menus{
-		Week:       "Thursday",
-		Higawari:   thu[1],
-		Donmono:    thu[2],
-		Fish:       thu[3],
-		Salad:      thu[4],
-		Dessert:    thu[5],
-		Onepin:     thu[6],
-		Pasta:      thu[7],
-		Menrui:     thu[8],
-		EveningSet: thu[9],
-	})
-
-	menu = append(menu, menus{
-		Week:       "Friday",
-		Higawari:   fri[1],
-		Donmono:    fri[2],
-		Fish:       fri[3],
-		Salad:      fri[4],
-		Dessert:    fri[5],
-		Onepin:     fri[6],
-		Pasta:      fri[7],
-		Menrui:     fri[8],
-		EveningSet: fri[9],
-	})
-
-	// fmt.Println(reflect.TypeOf(menu))
-
+	getCafe()
 	bytes, _ := json.Marshal(menu)
-	ioutil.WriteFile("./json/meshi.json", bytes, os.ModePerm)
+	ioutil.WriteFile("json/meshi.json", bytes, os.ModePerm)
 	fmt.Println("success")
 }
